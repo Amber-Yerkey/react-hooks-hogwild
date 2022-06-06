f534870fecad4b534c18feb2fe69ede90b0a68d8
Amber Frantz <amber.k.frantz@mgail.com>
2022-06-05 20:46:32 -0400

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
+import ListItem from "./ListItem"
+import Filter from "./Filter"
 
 import hogs from "../porkers_data";
 
 function App() {
+	//console.log(hogs)
+	const [greasedFilter, setGreasedFilter] = useState(false)
+	const [isSorted, setSort] = useState("name")
+
+	const filterHogs = hogs
+		.filter((hog) => (greasedFilter ? hog.greased : true))
+		.sort((fHog, sHog) =>{
+			if(isSorted === "weight"){
+				return fHog.weight - sHog.weight
+			} else {
+				return fHog.name.localeCompare(sHog.name)
+			}
+		})
+
 	return (
 		<div className="App">
 			<Nav />
+			<Filter greasedFilter={greasedFilter} onChangeGreaseFilter={setGreasedFilter} isSorted={isSorted} onChangeSort={setSort} />
+			<ListItem hogs={filterHogs}/>
 		</div>
 	);
 }
